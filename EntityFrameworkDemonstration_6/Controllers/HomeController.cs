e51bc45f374c27ed3ab15cf23e773be946c5a367
Utpal Maiti <Utpalmaiti12@gmail.com>
2020-07-05 02:13:46 +0530

    Add project files.

---
+++ EntityFrameworkDemonstration_6/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using EntityFrameworkDemonstration_6.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkDemonstration_6.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            PragimDBEntities dbContext = new PragimDBEntities();
+
+            return View(dbContext.EmpInfoes.ToList());
+        }
+
+        //Concurrency:   management
+        public ActionResult EditEmployee(int Id)
+        {
+            PragimDBEntities dbContext = new PragimDBEntities();
+
+            var employee = dbContext.EmpInfoes.Find(Id);
+
+            bool isLocked = employee.IsLocked ?? false;
+
+            ViewBag.IsLocked = isLocked;
+
+            if (!isLocked)
+            {
+                employee.IsLocked = true;
+
+                dbContext.SaveChanges();
+            }
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult EditEmployee(EmpInfo emp)
+        {
+            PragimDBEntities dbContext = new PragimDBEntities();
+
+            dbContext.Entry(emp).State = System.Data.Entity.EntityState.Modified;
+
+            dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
